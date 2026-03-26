7fb9fa16fbff6adb0aa5adbe4e10331a686a1525
Rael30-zucker <seilameuh30@gmail.com>
2026-03-26 15:31:51 -0300

    Update server.js

--- server.js
+++ server.js
@@ -109,4 +109,7 @@ app.delete('/aluno/:id', auth, async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log('Servidor rodando'));
+app.get('/', (req, res) => {
+  res.send('Servidor online 🚀');
+});
 
